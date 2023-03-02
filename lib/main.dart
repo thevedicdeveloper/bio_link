058b3794665f8b5bc6220c35454d5377c35af15d
The Vedic Developer <thevedicdev@gmail.com>
2023-03-01 20:26:47 -0500

    DAY02 - Completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main(){
 
 class BioLink extends StatelessWidget {
   const BioLink({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +18,16 @@ class BioLink extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-             Image.asset('assets/icon/profile.png')
+            //  Image.asset('profile.png'),
+            CircleAvatar(
+              backgroundImage: AssetImage('profile.png'),
+              radius: 75,
+              // child: Text('NK'),
+            ),
+            Row(
+              children: [Text('Nishant'), Image.network('https://www.flaticon.com/free-icon/quality_6294076?term=twitter+verified+symbol&page=1&position=1&origin=tag&related_id=6294076')],
+            ),
+            Text('Hi, I am Nishant 😉. Internet calls me the Vedic Developer 😎.'),
             ],
           ),
         ),
